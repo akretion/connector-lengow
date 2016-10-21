265c71235846a851437da1b77b0a063180777b9a
Cédric Pigeon <cedric.pigeon@acsone.eu>
2016-10-21 11:09:44 +0200

    Add module for Nature et Découvertes specifications

---
+++ lengowerpconnect-naturedecouvertes/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Cédric Pigeon
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Lengow Connector Nature et Découvertes",
+    "summary": "Module used to manage specification for Nature et Découvertes"
+        " marketplace",
+    "version": "8.0.1.0.0",
+    "category": "Connector",
+    "website": "https://odoo-community.org/",
+    "author": "Cédric Pigeon, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "lengowerpconnect",
+    ],
+    "data": [
+        'data/lengowerpconnect_data.xml',
+    ]
+}
